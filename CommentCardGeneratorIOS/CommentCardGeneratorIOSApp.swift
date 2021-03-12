f53448f2cf907fdcb7ac35f78fa49d69b4435cf7
Kit Ropner <ropner.k@etoncollege.org.uk>
2021-03-12 12:16:27 +0000

    Initial Commit

---
+++ CommentCardGeneratorIOS/CommentCardGeneratorIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardGeneratorIOSApp.swift
+//  CommentCardGeneratorIOS
+//
+//  Created by Ropner, Kit (NA) on 12/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardGeneratorIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
