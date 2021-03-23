03b1ba2619577aa59035aebf8e7528b6bca8c589
Kit Ropner <ropner.k@etoncollege.org.uk>
2021-03-23 09:42:39 +0000

    a

--- CommentCardGeneratorIOS/ContentView.swift
+++ CommentCardGeneratorIOS/ContentView.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var commentCard = CommentCard()
+        
     var body: some View {
-        Text(commentCard.generateCommentCard())
-            .padding()
+        VStack {
+            Text(commentCard.generateCommentCard())
+                .font(.callout)
+                .fontWeight(.black)
+                .foregroundColor(Color.green)
+                .multilineTextAlignment(.center)
+                .padding(.all)
+                .toolbar{
+                    EditButton()
+                }
+            if Button() {
+                ColorPicker("Colour picker", selection: .constant(.red))
+            } else {
+                /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+            }
+        }
     }
 }
 
---
+++ CommentCardGeneratorIOS/Menu.swift
@@ -0,0 +1,28 @@
+//
+//  SwiftUIView.swift
+//  CommentCardGeneratorIOS
+//
+//  Created by Ropner, Kit (NA) on 23/03/2021.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Choose menu option")
+    }
+    
+    struct Spacer{
+        
+    }
+    struct Toolbar{
+        
+    }
+    
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
